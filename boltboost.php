3cc13956a38b555744f046c1b1d1a3925fb71ce2
Risad212 <hafez.risad@gmail.com>
2025-08-08 19:14:54 +0600

    done

---
+++ boltboost.php
@@ -0,0 +1,81 @@
+<?php
+/*
+Plugin Name: BoltBoost
+Plugin URI: 
+Description: perfomance analying
+Version: 1.0.0
+Author: hmrisad
+Author URI: 
+License: GPL3
+License URI: http://www.gnu.org/licenses/gpl.html
+Text Domain: boltboost
+*/
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+// inclused folder
+include_once 'includes/Menu.php';
+
+final class BoltBoost {
+
+    /**
+     * define plugin version
+     * 
+     * @const
+     */
+     const version = '1.0.0';
+
+    /**
+    * Class construcotr
+    * 
+    * @access private
+    */
+    private function __construct() {
+        $this->define_constants();
+        
+        add_action( 'plugins_loaded', [ $this, 'plugin_setup' ] );
+    }
+
+    /**
+    * Initializes a singleton instance
+    *
+    * @access public
+    */
+    public static function plugin_init() {
+        static $instance = false;
+
+        if ( ! $instance ) {
+            $instance = new self();
+        }
+
+        return $instance;
+    }
+
+    /**
+    * Define the required plugin constants
+    *
+    * @access public
+    */
+    public function define_constants() {
+        define( 'BB_VERSION', self::version );
+        define( 'BB_FILE',    __FILE__ );
+        define( 'BB_PATH',    __DIR__ );
+        define( 'BB_URL',     plugins_url( '', BB_FILE ) );
+        define( 'BB_ASSETS',  BB_URL . '/assets' );
+    }
+
+    /**
+    * setup main class
+    *
+    * @access public
+    */
+    public function plugin_setup(){
+         if( is_admin() ){
+              new Menu();
+         }
+    }
+
+}
+BoltBoost::plugin_init();
---
+++ includes/Menu.php
@@ -0,0 +1,35 @@
+<?php
+
+
+class Menu {
+
+    function __construct() {
+        add_action( 'admin_menu', [ $this, 'admin_menu' ] );
+    }
+
+    /**
+     * Register admin menu
+     *
+     * @access public
+     */
+    public function admin_menu() {
+        add_menu_page(
+        __( 'BoltBoost', 'pedro-elementor-addons' ),  // Page title
+        __( 'BoltBoost', 'pedro-elementor-addons' ), // Menu title
+        'manage_options',                            // Capability
+        'boltboost',                                 // Menu slug
+        [ $this, 'plugin_page' ],                    // Callback function
+        'dashicons-vault'                            // Icon
+    );
+
+    }
+
+    /**
+     * Render the plugin page
+     *
+     * @access public
+     */
+    public function plugin_page() {
+        echo 'hello ';
+    }
+}
