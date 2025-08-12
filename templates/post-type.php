685675c1b10c9f965f72eafeaff37c4f9ceb0a71
Risad212 <hafez.risad@gmail.com>
2025-08-12 21:52:32 +0600

    done

--- templates/post-type.php
+++ templates/post-type.php
@@ -1,1 +1,129 @@
-<h2>hello</h2>
+<div id="bb-dashboard-post" class="bb-dashboard-section">
+    <h4 class="bb-section-title">
+        Post Type &amp; Metadata Overview
+    </h4>
+    <p class="bb-section-disc">
+        See how many posts, pages, and custom content types your site has—plus how much metadata is attached.
+        <br>
+        Quickly spot unused or bloated content types that may be slowing things down.
+    </p>
+    <div class="bb-desh-card-wrap">
+        <div class="bb-desh-card__single">
+            <span class="bb-desh-card__title">Post Types</span>
+            <span class="bb-desh-card__count">
+                3
+            </span>
+        </div>
+
+        <div class="bb-desh-card__single">
+            <span class="bb-desh-card__title">Total Items</span>
+            <span class="bb-desh-card__count">
+                <span>5</span>
+            </span>
+        </div>
+
+        <div class="bb-desh-card__single">
+            <span class="bb-desh-card__title">Total Meta</span>
+            <span class="bb-desh-card__count">
+                <span>4</span>
+            </span>
+        </div>
+
+        <div class="bb-desh-card__single">
+            <span class="bb-desh-card__title">Revisions</span>
+            <span class="bb-desh-card__count">
+                <span>0</span>
+            </span>
+        </div>
+    </div>
+
+    <div class="bb-dashboard-data">
+        <table class="bb-dashboard__table">
+            <thead>
+                <tr>
+                    <th>Post Type</th>
+                    <th>Total Items</th>
+                    <th>Total Meta</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Page</td>
+                    <td>
+                        <span class="bb-data-wrapper">
+                            <span class="bb-data-count">2</span>
+                            <span class="bb-data-progress-wrapper">
+                                <span class="bb-data-progress" style="width: 40%;"></span>
+                            </span>
+                        </span>
+                    </td>
+                    <td>
+                        <span class="bb-data-wrapper">
+                            <span class="bb-data-count">2</span>
+                            <span class="bb-data-progress-wrapper">
+                                <span class="bb-data-progress" style="width: 50%;"></span>
+                            </span>
+                        </span>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Post</td>
+                    <td>
+                        <span class="bb-data-wrapper">
+                            <span class="bb-data-count">1</span>
+                            <span class="bb-data-progress-wrapper">
+                                <span class="bb-data-progress" style="width: 20%;"></span>
+                            </span>
+                        </span>
+                    </td>
+                    <td>
+                        <span class="bb-data-wrapper">
+                            <span class="bb-data-count">0</span>
+                            <span class="bb-data-progress-wrapper">
+                                <span class="bb-data-progress" style="width: 0%;"></span>
+                            </span>
+                        </span>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Wp_navigation</td>
+                    <td>
+                        <span class="bb-data-wrapper">
+                            <span class="bb-data-count">1</span>
+                            <span class="bb-data-progress-wrapper">
+                                <span class="bb-data-progress" style="width: 20%;"></span>
+                            </span>
+                        </span>
+                    </td>
+                    <td>
+                        <span class="bb-data-wrapper">
+                            <span class="bb-data-count">0</span>
+                            <span class="bb-data-progress-wrapper">
+                                <span class="bb-data-progress" style="width: 0%;"></span>
+                            </span>
+                        </span>
+                    </td>
+                </tr>
+                <tr>
+                    <td>_orphaned_posts</td>
+                    <td>
+                        <span class="bb-data-wrapper">
+                            <span class="bb-data-count">1</span>
+                            <span class="bb-data-progress-wrapper">
+                                <span class="bb-data-progress" style="width: 20%;"></span>
+                            </span>
+                        </span>
+                    </td>
+                    <td>
+                        <span class="bb-data-wrapper">
+                            <span class="bb-data-count">0</span>
+                            <span class="bb-data-progress-wrapper">
+                                <span class="bb-data-progress" style="width: 0%;"></span>
+                            </span>
+                        </span>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
