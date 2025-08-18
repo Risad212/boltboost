30344cb06b9474c028de2fd765abb4f5ceac0b1b
Risad212 <hafez.risad@gmail.com>
2025-08-18 22:26:22 +0600

    done

--- includes/PostType.php
+++ includes/PostType.php
@@ -2,13 +2,9 @@
 
 class Post_Type{
 
-   /**
-    * Declear variable
-    * 
-    * @access public
-    */
-    public $get_total_meta_count = null;
-    public $get_total_revisions  = null; 
+   // Cache properties to store results once queried
+    public static $get_total_meta_count = null;
+    public static $get_total_revisions  = null; 
 
    /**
     * Get total meta count
@@ -16,12 +12,13 @@ class Post_Type{
     * @access public
     */
     public function get_total_meta(){
-      if( null !== $this->get_total_meta_count ){
-          return $this->get_total_meta_count;
+      if( null !== self::$get_total_meta_count ){
+          return self::$get_total_meta_count;
       }
+
       global $wpdb;
-		$this->get_total_meta_count = (int) $wpdb->get_var( "SELECT COUNT(*) FROM {$wpdb->postmeta}" );
-		return $this->get_total_meta_count;
+		  self::$get_total_meta_count = (int) $wpdb->get_var( "SELECT COUNT(*) FROM {$wpdb->postmeta}" );
+		 return self::$get_total_meta_count;
    }
 
    /**
@@ -30,12 +27,13 @@ class Post_Type{
     * @access public
     */
     public function get_total_revisions(){
-      if( null !== $this->get_total_revisions ){
-          return $this->get_total_revisions;
+      if( null !== self::$get_total_revisions ){
+          return self::$get_total_revisions;
       }
+
       global $wpdb;
-		$this->get_total_revisions = $wpdb->get_var( "SELECT COUNT(*) FROM {$wpdb->posts} WHERE post_type = 'revision'" );
-		return $this->get_total_revisions;
+		  self::$get_total_revisions = $wpdb->get_var( "SELECT COUNT(*) FROM {$wpdb->posts} WHERE post_type = 'revision'" );
+		  return self::$get_total_revisions;
    }
 }
 
