3e7f991b3f16941d9f9ad7a2f12e7c9d1288daca
Hafez Risad <83496132+Risad212@users.noreply.github.com>
2025-12-02 20:45:45 +0600

    Add database overview template
    added new fiels for render database ui

---
+++ templates/database.php
@@ -0,0 +1,39 @@
+<?php 
+  require_once BB_DIR_PATH . 'templates/store.php';
+?>
+
+<div id="bb-dashboard-post" class="bb-dashboard-section">
+    <h4 class="bb-section-title">
+        Database Overview
+    </h4>
+    <div class="bb-desh-card-wrap">
+        <div class="bb-desh-card__single">
+            <span class="bb-desh-card__title">DB Size</span>
+            <span class="bb-desh-card__count">
+ 
+            </span>
+        </div>
+
+        <div class="bb-desh-card__single">
+            <span class="bb-desh-card__title">Total Items</span>
+            <span class="bb-desh-card__count">
+            
+            </span>
+        </div>
+
+        <div class="bb-desh-card__single">
+            <span class="bb-desh-card__title">Total Meta</span>
+            <span class="bb-desh-card__count">
+           
+            </span>
+        </div>
+
+        <div class="bb-desh-card__single">
+            <span class="bb-desh-card__title">Revisions</span>
+            <span class="bb-desh-card__count">
+            
+            </span>
+        </div>
+    </div>
+   <!--- table start from here ---->
+</div>
