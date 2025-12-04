2b994e3a41683987167e3d483191d2b98f51c945
Hafez Risad <83496132+Risad212@users.noreply.github.com>
2025-12-04 20:41:16 +0600

    Update database.php
    update database ui code

--- templates/database.php
+++ templates/database.php
@@ -10,28 +10,28 @@
         <div class="bb-desh-card__single">
             <span class="bb-desh-card__title">DB Size</span>
             <span class="bb-desh-card__count">
- 
+               <?php echo $db_size; ?>
             </span>
         </div>
 
         <div class="bb-desh-card__single">
-            <span class="bb-desh-card__title">Total Items</span>
+            <span class="bb-desh-card__title">Tables</span>
             <span class="bb-desh-card__count">
-            
+              <?php echo $db_tables; ?>
             </span>
         </div>
 
         <div class="bb-desh-card__single">
-            <span class="bb-desh-card__title">Total Meta</span>
+            <span class="bb-desh-card__title">Options</span>
             <span class="bb-desh-card__count">
-           
+              <?php echo $db_option; ?>
             </span>
         </div>
 
         <div class="bb-desh-card__single">
-            <span class="bb-desh-card__title">Revisions</span>
+            <span class="bb-desh-card__title">Transients</span>
             <span class="bb-desh-card__count">
-            
+               <?php echo $db_transist; ?>
             </span>
         </div>
     </div>
