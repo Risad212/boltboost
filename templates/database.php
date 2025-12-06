e551cce0a1c24049d4880d7ca885a397f000d3a7
Hafez Risad <83496132+Risad212@users.noreply.github.com>
2025-12-06 19:27:52 +0600

    Update database.php
    udpated database.php ui files

--- templates/database.php
+++ templates/database.php
@@ -17,7 +17,7 @@
         <div class="bb-desh-card__single">
             <span class="bb-desh-card__title">Tables</span>
             <span class="bb-desh-card__count">
-              <?php echo $db_tables; ?>
+              <?php echo $db_total_tables; ?>
             </span>
         </div>
 
@@ -36,4 +36,30 @@
         </div>
     </div>
    <!--- table start from here ---->
+    <div class="ba-dashboard__content__section__data">
+    <table>
+        <thead>
+            <tr>
+                <th>Table Name</th>
+                <th>Rows</th>
+                <th>Data Size</th>
+                <th>Index Size</th>
+                <th>Total Size</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <?php foreach( $db_core_tables as $table ) { ?>
+               <tr>
+                 <td><?php echo $table['table_name'] ?></td>
+                 <td><?php echo $table['row_count'] ?></td>
+                 <td><?php echo $table['data_size_formatted'] ?></td>
+                 <td><?php echo $table['index_size_formatted'] ?></td>
+                 <td><?php echo $table['total_size_formatted'] ?></td>
+              </tr>
+            <?php } ?>
+        </tbody>
+    </table>
+</div>
+
 </div>
